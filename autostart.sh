6318f1e16f97cdadc534b2e5e2532a32ab3c7cd7
Elendil <elendil95@hotmail.it>
2022-05-21 11:05:42 +0200

    added basic autostart file and Makefile comands to copy it if missing

---
+++ autostart.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+slstatus &
+# picom -b --config ~/.config/compton/compton.conf &
+# wal -R
+# xsettingsd &
+# xfce4-power-manager &
+# volumeicon &
+# pa-applet &
+nm-applet &
+# syncthing-gtk -m &
+# redshift-gtk &
+setxkbmap it
+light-locker --lock-on-suspend &
+# /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+# exec expressvpn connect &
+# wmname "LG3D"
